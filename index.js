2779d3a251d515d7811e3168e40d81f4f3b6fd04
jofiab <61145711+jofiab@users.noreply.github.com>
2023-02-17 22:06:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
-const port = 3001;
+const port = 80;
 const dotenv = require('dotenv')
 
 dotenv.config();
